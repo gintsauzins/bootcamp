de6987df398ea0bcc974e95a91880cce116c7b41
gintsauzins <gints.job@gmail.com>
2022-08-12 10:47:44 +0300

    dom js

---
+++ TESTS/dom.js
@@ -0,0 +1,3 @@
+let template_btn = document.querySelector('.button'); //document - viss dokuments lapaa un no vina ar querySelector - metodi(selektoru) , ar kuru var atlasit. šajā gadijumā button no index faila
+//izvadiju mainigaja lai varetu console logaa paskatities ko vins izvada
+console.log(document.querySelector('.button'));
